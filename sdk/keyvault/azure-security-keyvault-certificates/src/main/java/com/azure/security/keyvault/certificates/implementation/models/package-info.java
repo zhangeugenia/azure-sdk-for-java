d33bf6f49739a43726ef3b5cf4bcf68c99306502
SDKAuto <sdkautomation@microsoft.com>
2025-04-23 22:27:11 +0000

    CodeGen from PR 34032 in Azure/azure-rest-api-specs Merge c05749c07658b43406ee1632c47ff96074aa5137 into 4689e75cdeaa04dbeead0237b72cae8dc8dbba4d

--- sdk/keyvault/azure-security-keyvault-certificates/src/main/java/com/azure/security/keyvault/certificates/implementation/models/package-info.java
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-/**
- * <!-- @formatter:off -->
- * Package containing the implementation data models for Certificates clients. The key vault client performs
- * cryptographic key operations and vault operations against the Key Vault service.
- */
-package com.azure.security.keyvault.certificates.implementation.models;
--- sdk/keyvault/azure-security-keyvault-certificates/src/main/java/com/azure/security/keyvault/certificates/implementation/package-info.java
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-/**
- * <!-- @formatter:off -->
- * Package containing the implementations for Certificates clients. The key vault client performs cryptographic key
- * operations and vault operations against the Key Vault service.
- */
-package com.azure.security.keyvault.certificates.implementation;
