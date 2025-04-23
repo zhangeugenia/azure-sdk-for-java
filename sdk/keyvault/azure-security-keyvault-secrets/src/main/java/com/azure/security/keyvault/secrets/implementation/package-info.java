661d5cfa758ff771cf86b504617eaddc9f221d15
SDKAuto <sdkautomation@microsoft.com>
2025-04-23 22:30:04 +0000

    CodeGen from PR 34032 in Azure/azure-rest-api-specs Merge c05749c07658b43406ee1632c47ff96074aa5137 into 4689e75cdeaa04dbeead0237b72cae8dc8dbba4d

--- sdk/keyvault/azure-security-keyvault-secrets/src/main/java/com/azure/security/keyvault/secrets/implementation/package-info.java
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-/**
- * <!-- @formatter:off -->
- * Package containing the implementations for Secrets clients. The Key Vault clients perform cryptographic key
- * operations and vault operations against the Key Vault service.
- */
-package com.azure.security.keyvault.secrets.implementation;
